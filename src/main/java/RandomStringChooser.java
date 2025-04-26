0705d38054dbc3c0cde4ebcad66ec9e4c02910ee
ManuelAlcaz101 <111898585+ManuelAlcaz101@users.noreply.github.com>
2025-04-25 23:26:22 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -6,15 +6,15 @@ public class RandomStringChooser
   private int valuesRemaining;
   public RandomStringChooser (String [] vals){
     values = new String[vals.length];
-    for (int i = 0; i < values.length; i++)
-      values[i］= vals (i);
+    for(int i = 0; i < values.length;i++)
+      values[i] = vals(i);
     valuesRemaining = values.length;
 }
   public String getNext(){
-    if (valuesRemaining == 0)
+    if(valuesRemaining == 0)
       return "NONE";
-    int index = (int) (Math. random() * valuesRemaining);
-    String selected = values [index];
+    int index = (int) (Math.random() * valuesRemaining);
+    String selected = values[index];
     values (index] = values [valuesRemaining - 1];
     valuesRemaining--; 
     return selected;
